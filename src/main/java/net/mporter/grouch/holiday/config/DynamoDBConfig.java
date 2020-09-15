fe2a38f6e3a41ad5f82e801049e1f5108752a6cd
Matt Porter <matthew.js.porter@gmail.com>
2020-09-15 00:58:05 -0400

    fixing DynamoDBConfig

--- src/main/java/net/mporter/grouch/holiday/config/DynamoDBConfig.java
+++ src/main/java/net/mporter/grouch/holiday/config/DynamoDBConfig.java
@@ -2,12 +2,15 @@ package net.mporter.grouch.holiday.config;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import net.mporter.grouch.holiday.repository.HolidayRepository;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("!local")
+@EnableDynamoDBRepositories(basePackageClasses = HolidayRepository.class)
 public class DynamoDBConfig {
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
