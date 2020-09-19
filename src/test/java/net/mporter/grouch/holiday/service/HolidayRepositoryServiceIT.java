ecd40b2a6918e693aec41fbc98c8f11b905fe33b
Matt Porter <matthew.js.porter@gmail.com>
2020-09-19 09:10:39 -0400

    moved creating table to template.yml

--- src/test/java/net/mporter/grouch/holiday/service/HolidayRepositoryServiceIT.java
+++ src/test/java/net/mporter/grouch/holiday/service/HolidayRepositoryServiceIT.java
@@ -1,16 +1,22 @@
 package net.mporter.grouch.holiday.service;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import net.mporter.grouch.holiday.Application;
 import net.mporter.grouch.holiday.model.Holiday;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.PostConstruct;
 
 import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
@@ -23,6 +29,9 @@ public class HolidayRepositoryServiceIT {
 
     @Autowired
     private AmazonDynamoDB amazonDynamoDB;
+
+    @Autowired
+    private DynamoDBMapper dynamoDBMapper;
 
     private Holiday holiday;
 
@@ -67,5 +76,22 @@ public class HolidayRepositoryServiceIT {
     private void testDelete() {
         holidayService.delete(holiday);
         assertNull(holidayService.fetchHoliday(holiday.getName()));
+    }
+
+    @PostConstruct
+    private void createHolidayTable() {
+        if (!isTable("Holiday")) {
+            amazonDynamoDB.createTable(createTableRequest());
+        }
+    }
+
+    private CreateTableRequest createTableRequest() {
+        return dynamoDBMapper.generateCreateTableRequest(Holiday.class)
+                .withProvisionedThroughput(
+                        new ProvisionedThroughput(5L, 5L)
+                ).withTableName("Holiday");
+    }
+    private boolean isTable(final String table) {
+        return amazonDynamoDB.listTables().getTableNames().contains(table);
     }
 }
